776bbc687e4ebe855fedd1138ab11392e8fe0015
Yotam Mann <yotammann@gmail.com>
2015-07-13 21:48:38 -0400

    parse the output with a "settings" section

--- index.js
+++ index.js
@@ -15,111 +15,111 @@
  */
 
 var argv = require('minimist')(process.argv.slice(2));
-console.dir(argv);
-
 var midiConverter = require("midi-converter");
 var fs = require("fs");
 
-var file = fs.readFileSync(process.argv[2], "binary");
+/**
+ *  MIDI ticks. should be read from the file. 
+ */
+var ticksPerBeat = 480;
 
-var midi = midiConverter.midiToJson(file);
-
-
-var output = {};
-
-var settings = {};
-
+/**
+ *  the default settings
+ */
 var defaultSettings = {
 	PPQ : 48,
 	tempo : 120,
 	timeSignature : [4, 4],
 };
 
-var tempo = 120;
-var TonePPQ = 12;
-var ticksPerBeat = midi.header.ticksPerBeat;
-var timeSignature = [4, 4];
-var microsecondsPerBeat = 60000000 / tempo;
-
-//parse the tracks
-for (var i = 0; i < midi.tracks.length; i++) {
-	var data = midi.tracks[i];
-	var trackName = "track"+i;
-	var trackNotes = [];
-	var currentTime = 0;
-	for (var j = 0; j < data.length; j++) {
-		//meta tag stuff
-		var evnt = data[j];
-		currentTime += evnt.deltaTime;
-		if (evnt.type === "meta"){
-			if (evnt.subtype === "timeSignature"){
-				timeSignature[0] = evnt.numerator;
-				timeSignature[1] = evnt.denominator;
-			} else if (evnt.subtype === "setTempo"){
-				tempo = 60000000 / evnt.microsecondsPerBeat;
-				microsecondsPerBeat = evnt.microsecondsPerBeat;
-			} else if (evnt.subtype === "trackName"){
-				trackName = evnt.text;
+/**
+ *  Get all of the settings out of the tracks
+ */
+function parseSettings(midi){
+	var settings = {};
+	for (var i = 0; i < midi.tracks.length; i++) {
+		var track = midi.tracks[i];
+		var trackNotes = [];
+		for (var j = 0; j < track.length; j++){
+			var evnt = track[j];
+			if (evnt.type === "meta"){
+				if (evnt.subtype === "timeSignature"){
+					settings.timeSignature = [evnt.numerator, evnt.denominator];
+				} else if (evnt.subtype === "setTempo"){
+					settings.tempo = 60000000 / evnt.microsecondsPerBeat;
+				} else if (evnt.subtype === "trackName"){
+					trackName = evnt.text;
+				}
 			}
-		} else {
-			var time = deltaTimeToMeter(currentTime);
+		}
+	}
+	//also go through and grab the MIDI PPQ
+	ticksPerBeat = midi.header.ticksPerBeat;
+	for (var attr in defaultSettings){
+		if (typeof settings[attr] === "undefined"){
+			settings[attr] = defaultSettings[attr];
+		}
+	}
+	return settings;
+}
+
+/**
+ *  Convert 
+ */
+function parseTracks(midi){
+	var output = {};
+	for (var i = 0; i < midi.tracks.length; i++) {
+		var track = midi.tracks[i];
+		var trackName = "track"+i;
+		var trackNotes = [];
+		var currentTime = 0;
+		for (var j = 0; j < track.length; j++){
+			var evnt = track[j];
+			currentTime += evnt.deltaTime;
 			var note = midiToNote(evnt.noteNumber);
 			var velocity = evnt.velocity / 127;
 			if (evnt.subtype === "noteOn"){
 				trackNotes.push({
 					"ticks" : currentTime,
-					"time" : time, 
+					"time" : currentTime, 
 					"note" : note, 
-					"velocity" : velocity
+					"velocity" : velocity,
+					"midiNote" : evnt.noteNumber
 				});
 			} else if (evnt.subtype === "noteOff"){
 				//add the duration
-				for (var i = trackNotes.length - 1; i >= 0; i--){
-					var trackNote = trackNotes[i];
+				for (var k = trackNotes.length - 1; k >= 0; k--){
+					var trackNote = trackNotes[k];
 					if (trackNote.note === note && typeof trackNote.duration === "undefined"){
-						trackNote.duration = deltaTimeToMeter(currentTime - trackNote.ticks);
+						trackNote.duration = currentTime - trackNote.ticks;
 						delete trackNote.ticks;
 						break;
 					}
 				}
-			} 
+			} else if (evnt.type === "meta" && evnt.subtype === "trackName"){
+				trackName = evnt.text;
+			}
+		}
+		output[trackName] = trackNotes;
+	}
+	return output;
+}
+
+/**
+ *  Convert MIDI PPQ into Tone.js PPQ
+ */
+function convertTicksToPPQ(tracks, PPQ){
+	for (var trackName in tracks){
+		var track = tracks[trackName];
+		for (var note = 0; note < track.length; note++){
+			track[note].time = ticksToToneTicks(track[note].time, PPQ);
+			track[note].duration = ticksToToneTicks(track[note].duration, PPQ);
 		}
 	}
-	output[trackName] = trackNotes;
 }
 
-output.tempo = tempo;
-output.timeSignature = timeSignature;
-
-//write it to the output file
-fs.writeFileSync(process.argv[2]+".json", JSON.stringify(output));
-
-function deltaTimeToMeter(deltaTime){
-	var timeSigValue = timeSignature[0] / (timeSignature[1] / 4);
-	// return deltaTime;
-	var quarters = deltaTime / ticksPerBeat;
-	return quarters * 12;
-
-	//old
-	var measures = Math.floor(quarters / timeSigValue);
-	var sixteenths = (quarters % 1) * 4;
-	quarters = Math.floor(quarters) % timeSigValue;
-	var progress = [measures, quarters, sixteenths];
-	return progress.join(":");
-}
-
-/**
- *  Convert MIDI PPQ into Tone.js PPQ
- */
-function parseSettings(midi){
-	return Math.round((ticks / ticksPerBeat) * TonePPQ) + "i";
-}
-
-/**
- *  Convert MIDI PPQ into Tone.js PPQ
- */
-function PPQtoTonePPQ(ticks){
-	return Math.round((ticks / ticksPerBeat) * TonePPQ) + "i";
+function ticksToToneTicks(tick, PPQ){
+	return Math.round((tick / ticksPerBeat) * PPQ) + "i";
 }
 
 /**
@@ -131,3 +131,22 @@ function midiToNote(noteNum){
 	var note = noteNum % 12;
 	return noteIndexToNote[note] + octave;
 }
+
+/**
+ *  the main parsing function
+ */
+(function main(){
+	//read the file
+	var file = fs.readFileSync(process.argv[2], "binary");
+	//parse the midi
+	var midi = midiConverter.midiToJson(file);
+	//grab the settings
+	var settings = parseSettings(midi);
+	//get the tracks
+	var tracks = parseTracks(midi);
+	//convert the given ticks to Tone's PPQ
+	convertTicksToPPQ(tracks, settings.PPQ);
+	tracks.settings = settings;
+	//write the output
+	fs.writeFileSync(process.argv[2]+".json", JSON.stringify(tracks));
+}());
