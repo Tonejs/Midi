0223ed800ac53dcf701e4b88bd4279ca86df0ffb
Joseph Weidinger <jsphweid@gmail.com>
2018-03-14 21:01:07 -0500

    put control change under Track where it should be and format to .editorconfig and ts

--- src/MidiConvert.d.ts
+++ src/MidiConvert.d.ts
@@ -1,61 +1,60 @@
 export interface Note {
-	time: number,
-	name: string,
-	midi: number,
-	velocity: number,
-	duration: number,
+  time: number
+  name: string
+  midi: number
+  velocity: number
+  duration: number
 }
 
 export interface Track {
-	id?: number,
-	readonly isPercussion: boolean,
-	channelNumber: number,
-	name: string,
-	instrument: string,
-	instrumentNumber: number,
-	readonly instrumentFamily: string,
-	notes: Array<Note>,
-	readonly startTime: number,
-	readonly duration: number,
-	readonly length: number,
+  id?: number
+  readonly isPercussion: boolean
+  channelNumber: number
+  name: string
+  instrument: string
+  instrumentNumber: number
+  readonly instrumentFamily: string
+  notes: Array<Note>
+  readonly startTime: number
+  readonly duration: number
+  readonly length: number
+  controlChanges: {
+    [key: number]: ControlChange
+  }
 }
 
 export interface ControlChange {
-	readonly name: string,
-	number: number,
-	time: string,
-	value: number,
+  readonly name: string
+  number: number
+  time: string
+  value: number
 }
 
 export interface MIDI {
-	header: {
-		name: string,
-		bpm: number,
-		timeSignature: [number, number],
-		PPQ: number,
-	},
+  header: {
+    name: string
+    bpm: number
+    timeSignature: [number, number]
+    PPQ: number
+  }
 
-	readonly startTime: number,
-	readonly duration: number,
+  readonly startTime: number
+  readonly duration: number
 
-	tracks: Array<Track>,
+  tracks: Array<Track>
 
-	controlChanges: {
-		[key: number]: ControlChange
-	},
-
-	toJSON(): MIDI,
+  toJSON(): MIDI
 }
 
-export function parse(raw: ArrayBuffer|string): MIDI;
-export function load(url: string, data?: any, method?: 'GET'|'POST'): Promise<MIDI>;
-export function create(): MIDI;
-export function fromJSON(json: object): MIDI;
+export function parse(raw: ArrayBuffer | string): MIDI
+export function load(url: string, data?: any, method?: 'GET' | 'POST'): Promise<MIDI>
+export function create(): MIDI
+export function fromJSON(json: object): MIDI
 
 export interface StringsByID {
-	[index: number]: string;
+  [index: number]: string
 }
 
-export const instrumentByPatchID: StringsByID;
-export const instrumentFamilyByID: StringsByID;
-export const drumKitByPatchID: StringsByID;
+export const instrumentByPatchID: StringsByID
+export const instrumentFamilyByID: StringsByID
+export const drumKitByPatchID: StringsByID
